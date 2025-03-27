5e6aaeb40b0e0b227de7390899d64d8fb6f5f183
DiegoTaynan <diegotaynan13@gmail.com>
2025-03-27 14:03:25 -0300

    Tirar os logs

--- src/controllers/controller.banner.js
+++ src/controllers/controller.banner.js
@@ -3,7 +3,6 @@ import serviceBanner from "../services/service.banner.js";
 async function Listar(req, res) {
   try {
     const banners = await serviceBanner.Listar();
-    console.log("Banners do banco de dados:", banners); // Adicione o log
 
     res.status(200).json(banners);
   } catch (error) {
