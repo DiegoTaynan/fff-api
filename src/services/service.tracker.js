907109e262e40e022dcb90656231ccecafe17754
DiegoTaynan <diegotaynan13@gmail.com>
2025-04-14 18:34:58 -0300

    Branch ruim

--- src/services/service.tracker.js
+++ src/services/service.tracker.js
@@ -1,11 +1,8 @@
 import repositoryTracker from "../repositories/repository.tracker.js";
 
 async function Listar(id_user) {
-  console.log("Service: Fetching trackers for user:", id_user); // 🔥 Log para depuração
-
   const tracker = await repositoryTracker.Listar(id_user);
 
-  console.log("Service: Trackers fetched:", tracker); // 🔥 Log para verificar os dados retornados
   return tracker;
 }
 
